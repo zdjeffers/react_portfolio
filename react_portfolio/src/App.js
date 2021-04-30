3e5f6d10ae11704726a4a9c76675424304081530
Dave Jeffers <72349891+zdjeffers@users.noreply.github.com>
2021-04-29 20:41:17 -0400

    updated projects.json file, cleaned out unused photos

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
+import Projects from './pages/projects';
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Switch>
+            <Route exact path={["/"]}>
+              <Home />
+            </Route>
+            <Route exact path={["/home"]}>
+              <Home />
+            </Route>
+            <Route exact path={["/projects"]}>
+              <Projects />
+            </Route>
+          </Switch>
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
---
+++ react_portfolio/src/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Col, Row, Container } from "../components/Grid";
+
+function Home() {
+  return (
+    <div> home </div>
+    );
+}
+
+export default Home;
